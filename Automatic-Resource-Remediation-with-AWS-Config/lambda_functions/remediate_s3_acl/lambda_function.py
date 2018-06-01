ed7e87e79bf102fc7ff5b4ed709a2e6f46836af1
mrichman <mark@markrichman.com>
2018-06-01 14:35:01 -0400

    Initial revision

---
+++ Automatic-Resource-Remediation-with-AWS-Config/lambda_functions/remediate_s3_acl/lambda_function.py
@@ -0,0 +1,65 @@
+"""
+Lambda function to poll Config for noncompliant resources, and automatically 
+apply remediation by removing S3 bucket ACLs which allow public read. 
+Notifications are sent to an SNS topic.
+"""
+
+import boto3
+from pprint import pprint
+
+# AWS Config settings
+ACCOUNT_ID = boto3.client('sts').get_caller_identity()['Account']
+CONFIG_CLIENT = boto3.client('config')
+MY_RULE = "s3-bucket-public-read-prohibited"
+
+# AWS SNS Settings
+SNS_CLIENT = boto3.client('sns')
+SNS_TOPIC = "arn:aws:sns:us-east-1:%s:config-rules-compliance" % ACCOUNT_ID
+SNS_SUBJECT = 'Compliance Update'
+
+# AWS S3 Settings
+S3_CLIENT = boto3.client('s3')
+ALL_USERS_GROUP = 'http://acs.amazonaws.com/groups/global/AllUsers'
+
+
+def lambda_handler(event, context):
+    """Entry point"""
+
+    # Get compliance details
+    non_compliant_detail = CONFIG_CLIENT.get_compliance_details_by_config_rule(
+        ConfigRuleName=MY_RULE, ComplianceTypes=['NON_COMPLIANT'])
+
+    if len(non_compliant_detail['EvaluationResults']) > 0:
+
+        print(
+            'The following resource(s) are not compliant with AWS Config rule: ' + MY_RULE)
+
+        non_complaint_resources = ''
+
+        for result in non_compliant_detail['EvaluationResults']:
+            print(result['EvaluationResultIdentifier']
+                  ['EvaluationResultQualifier']['ResourceId'])
+            non_complaint_resources = non_complaint_resources + \
+                result['EvaluationResultIdentifier']['EvaluationResultQualifier']['ResourceId'] + '\n'
+
+        sns_message = 'AWS Config Compliance Update\n\n Rule: ' \
+            + MY_RULE + '\n\n' \
+            + 'The following resource(s) are not compliant:\n' \
+            + non_complaint_resources
+
+        SNS_CLIENT.publish(TopicArn=SNS_TOPIC,
+                           Message=sns_message, Subject=SNS_SUBJECT)
+
+        resource_type = result['EvaluationResultIdentifier']['EvaluationResultQualifier']['ResourceType']
+
+        if resource_type == 'AWS::S3::Bucket':
+            bucket_name = result['EvaluationResultIdentifier']['EvaluationResultQualifier']['ResourceId']
+            print('Bucket: ' + bucket_name)
+            s3 = boto3.resource('s3')
+            bucket = s3.Bucket(bucket_name)
+            acl = bucket.Acl()
+            response = acl.put(ACL='private')
+            pprint(response)
+
+    else:
+        print('No noncompliant resources detected.')
