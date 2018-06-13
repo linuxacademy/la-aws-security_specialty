adc6e38764db66f051cbff3e10a802772b36b447
mrichman <mark@markrichman.com>
2018-06-13 11:04:36 -0400

    Updated Lambda function

--- Troubleshooting-Detection-Alerting-Response-Workflow/lambda_function.py
+++ Troubleshooting-Detection-Alerting-Response-Workflow/lambda_function.py
@@ -1,15 +1,23 @@
 import boto3
 import json
 
+
 def lambda_handler(event, context):
+    '''
+    Extract the EC2 instance ID from the Alarm Description, and stop the
+    instance.
+    '''
 
-    json_message = event['Message']
-    message = json.loads(json_message)
-    instance = message['AlarmDescription'].split()[-1]
-    
-    print("Instance: " + instance)
-    
-    ec2 = boto3.client('ec2')
-    ec2.stop_instances(InstanceIds=[instance])
+    try:
+        json_message = event['Message']
+        message = json.loads(json_message)
+        instance = message['AlarmDescription'].split()[-1]
 
-    print('Stopped instance: ' + instance)
+        print("Instance: " + instance)
+
+        ec2 = boto3.client('ec2')
+        ec2.stop_instances(InstanceIds=[instance])
+
+        print('Stopped instance: ' + instance)
+    except Exception as e:
+        print('Error: ' + e)
